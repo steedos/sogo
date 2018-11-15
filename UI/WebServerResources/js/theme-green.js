8698a8fa810615f627b94ea9d4b8cf8e1fbc626a
Jack Zhuang <zhuangjianguo@me.com>
2018-11-15 19:10:03 +0800

    add themes

--- UI/WebServerResources/js/theme-green.js
+++ UI/WebServerResources/js/theme-green.js
@@ -14,13 +14,13 @@
 
 
     $mdThemingProvider.theme('default')
-      .primaryPalette('blue', {
+      .primaryPalette('green', {
         'default': '700',  // top toolbar
         'hue-1': '500',
         'hue-2': '700',    // sidebar toolbar
         'hue-3': 'A200'
       })
-      .accentPalette('blue', {
+      .accentPalette('green', {
         'default': '800',  // fab buttons
         'hue-1': '50',     // center list toolbar
         'hue-2': '500',
--- UI/WebServerResources/js/theme-orange.js
+++ UI/WebServerResources/js/theme-orange.js
@@ -14,19 +14,19 @@
 
 
     $mdThemingProvider.theme('default')
-      .primaryPalette('blue', {
+      .primaryPalette('green', {
         'default': '700',  // top toolbar
         'hue-1': '500',
         'hue-2': '700',    // sidebar toolbar
         'hue-3': 'A200'
       })
-      .accentPalette('blue', {
+      .accentPalette('amber', {
         'default': '800',  // fab buttons
         'hue-1': '50',     // center list toolbar
         'hue-2': '500',
         'hue-3': 'A700'
       })
-      .backgroundPalette('grey', {
+      .backgroundPalette('amber', {
         'default': '50',   // center list background
         'hue-1': '100',
         'hue-2': '200',
