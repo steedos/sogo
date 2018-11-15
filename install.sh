7cc4c03ea2a2d46ad76b2713be48930bdd4cee22
Jack Zhuang <zhuangjianguo@me.com>
2018-11-15 16:38:44 +0800

    纠正翻译

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ cp -r UI/WebServerResources/js /usr/lib64/GNUstep/SOGo/WebServerResources
 cp -r UI/Templates /usr/lib64/GNUstep/SOGo
 
 cp -r -f UI/MainUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/MainUI.SOGo/Resources
+cp -r -f UI/MailerUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/MailerUI.SOGo/Resources
 
 service sogod restart
