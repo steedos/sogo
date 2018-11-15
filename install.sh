90538c5196e6b3d856e2e40929475babd62b19bb
Jack Zhuang <zhuangjianguo@me.com>
2018-11-15 16:07:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-cd UI/WebServerResources
-cp -r css fonts img js /usr/lib64/GNUstep/SOGo/WebServerResources
+cp -r UI/WebServerResources/css /usr/lib64/GNUstep/SOGo/WebServerResources
+cp -r UI/WebServerResources/fonts /usr/lib64/GNUstep/SOGo/WebServerResources
+cp -r UI/WebServerResources/img /usr/lib64/GNUstep/SOGo/WebServerResources
+cp -r UI/WebServerResources/js /usr/lib64/GNUstep/SOGo/WebServerResources
 
+cp -r UI/Templates /usr/lib64/GNUstep/SOGo
+
+cp -r -f UI/MainUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/MainUI.SOGo/Resources
+
+service sogod restart
