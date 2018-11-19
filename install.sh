18d0851c7a7d65c1ac1af2e8df3d162f44d1feaf
Jack Zhuang <zhuangjianguo@me.com>
2018-11-19 10:57:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,7 @@ cp -r UI/Templates /usr/lib64/GNUstep/SOGo
 
 cp -r -f UI/MainUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/MainUI.SOGo/Resources
 cp -r -f UI/MailerUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/MailerUI.SOGo/Resources
+cp -r -f UI/PreferencesUI/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/PreferencesUI.SOGo/Resources
+cp -r -f UI/Scheduler/ChineseChina.lproj /usr/lib64/GNUstep/SOGo/Scheduler.SOGo/Resources
 
 service sogod restart
